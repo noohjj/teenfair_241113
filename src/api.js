397c903686d46c906714aade63aece806b9cb1fc
choseongchan3712 <choseongchan3712@naver.com>
2024-11-13 16:26:58 +0900

    push

--- src/api.js
+++ src/api.js
@@ -1,9 +1,14 @@
 const fetch = require("node-fetch");
 
-const baseUrl = "http://apis.data.go.kr/1383000/gmis/teenRAreaServiceV2"
+const baseUrl = "http://apis.data.go.kr/1383000/gmis/teenRAreaServiceV2";
 const options = {
-    method:"GET",
-    headers: {
-        accept: "*/*"
-    }
-}
+  method: "GET",
+  headers: {
+    accept: "*/*",
+    authorization:
+      "1l64t6Bdj/ETCziwk1dAKNoPCXX+U77BE9UDSsFYQmEfO7+e79daO3HTK5T6dFPGIB9PNzP7+Jlqq1j3YbcBiA==",
+  },
+};
+
+export const areaList = () =>
+  fetch(baseUrl("/getTeenRAreaList"), options).then((res) => res.json());
